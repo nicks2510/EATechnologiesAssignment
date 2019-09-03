ce633d96d2789ae28ea5206a7a9b117ad38c7f4a
Nikhil <nikhil.gupta@newgen.co.in>
2019-09-03 22:11:52 +0530

    second commit

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,28 +1,59 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import WebApi from "../WebApi/WebApi";
 
 
-function SearchBar() {
+function SearchBar(e) {
     const [employeSearchValue, setemployeSearchValue] = useState('');
-    async function fetchData(employeSearchValue){
-    try {
-        let response = await WebApi.checkEmployees();
-    if (response.status == 200) {
-        let employeeJson = await response.json(); // (3)
-        if(employeeJson.indexOf(employeSearchValue)>-1){
-            console.log(1);
-        };
-      }
-    } catch (error) {
-        
-    }
-    
+    const [empDataObj, setEmpDataObj] = useState({});
 
-}
-    
+    async function fetchData() {
+        try {
+            let response = await WebApi.checkEmployees();
+            if (response.status === 200) {
+                let employeeJson = await response.json(); 
+                if (employeeJson.indexOf(employeSearchValue) > -1) {
+                    let employeedata=[];
+                    employeedata=[...employeedata,employeSearchValue]
+                    getEmployeeSubs(employeedata, 0,{});
+                }
+                else {
+                    alert("No record found");
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+
+        async function getEmployeeSubs(employeedata, i,dataObj) {
+            if(employeedata[i]){
+                let response = await WebApi.getEmployeeData(employeedata[i]);
+                if (response.status === 200) {
+                    let employeeJson = await response.json();
+                    console.log(employeeJson);
+                    if (employeeJson.length === 1) {
+                        dataObj={...dataObj,[employeedata[i]]:''}
+                        getEmployeeSubs(employeedata, i + 1,dataObj)
+                    }
+                    if (employeeJson.length > 1) {
+                        let nonDirectSub = employeeJson[1]["direct-subordinates"];
+                        dataObj={...dataObj,[employeedata[i]]:nonDirectSub};
+                        employeedata = [...new Set([...employeedata, ...nonDirectSub])]
+                        getEmployeeSubs(employeedata, i + 1,dataObj)
+                    }
+                }
+            }
+            else{
+                console.log(dataObj)
+                setEmpDataObj(dataObj);
+            }
+          
+        }
+
+    }
+
     return (
         <div>
-            <input type="text" placeholder="Search Employee" onChange={(e)=>setemployeSearchValue(e.target.value)} value={employeSearchValue}/>
+            <input type="text" placeholder="Search Employee" onChange={(e) => setemployeSearchValue(e.target.value)} value={employeSearchValue} />
             <span >
                 <button type="button" onClick={fetchData}>Search</button>
             </span>
--- src/WebApi/WebApi.js
+++ src/WebApi/WebApi.js
@@ -9,6 +9,14 @@ const url='http://api.additivasia.io/api/v1/assignment/';
                 "Content-Type": "application/json"
             },
         })
+    },
+    getEmployeeData(employeeName){
+        return fetch(`${url}employees/${employeeName}`,{
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json"
+            },
+        })
     }
 }
 
